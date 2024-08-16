b0a8e4c356899da212b9c0ba81ea709624d976d7
Sadia-Shakoor <sp23-bai-047@cuilahore.edu.pk>
2024-08-16 15:41:52 +0500

    connection build

--- test.py
+++ test.py
@@ -1,1 +1,12 @@
-print('hey')
+import pandas
+import pyodbc
+
+conn_string="Driver=ODBC Driver 18 for SQL Server;Server=tcp:server-ha-ya.database.windows.net,1433;Database=database-ha-ya;Uid=UserID-ha-ya;Pwd=P@ssword-ha-ya;Encrypt=yes;TrustServerCertificate=no;Connection Timeout=30;"
+connection=pyodbc.connect(conn_string)
+connection.execute("""Create table ttable_1(
+                id int ,
+                name varchar(50))  """)
+
+connection.execute("Insert into table_1 (id,name)  values (1,'Amanat'), (2,'Asad'), (3,'Abdullah') ")
+connection.commit()
+print("hey")
